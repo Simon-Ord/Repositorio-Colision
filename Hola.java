d7b93b5f91076754bf14f7fcfd9647da7ca9c4d8
RicardoArg23 <ricardoarguello2312@gmail.com>
2025-08-06 17:45:28 -0300

    holaM3.0.1
    hola esta version es la mejor de todas

--- Hola.java
+++ Hola.java
@@ -2,9 +2,9 @@ public class Hola {
     
     public static void main(String[] args) {
         System.out.println("Hola mundeli");
-        System.out.println("colisionemos muajajaja");
     }
 }
 /*           
 Comentario de varias líneas
+
 */
